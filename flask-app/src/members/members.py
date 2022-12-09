53c21e8d9a9f8a0584f8aa058d4ea5d099ce4d92
utama.r <utama.r@northeastern.edu>
2022-12-09 13:06:12 -0500

    teams wireframe completed

--- flask-app/src/members/members.py
+++ flask-app/src/members/members.py
@@ -12,7 +12,7 @@ members = Blueprint('members', __name__)
 def get_members():
     cursor = db.get_db().cursor()
 
-    cursor.execute('select ID from members')
+    cursor.execute('select * from members')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -21,4 +21,62 @@ def get_members():
     the_response = make_response(jsonify(json_data))
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
+    return the_response
+
+# Get ALL teams info from the database
+
+
+@members.route('/members/teams', methods=['GET'])
+def get_teams():
+
+    # create cursor
+    cursor = db.get_db().cursor()
+
+    # create query
+    query = """select Experience_level, Current_capacity, Age_group, Days_of_week, 
+                First_name, Last_name
+                from teams join employees e on e.ID = teams.Coach_ID;"""
+
+    # execute
+    cursor.execute(query)
+
+    # return data as a json
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+# Get one team's info from the database
+
+
+@members.route('/members/teams/<id>', methods=['GET'])
+def get_one_team_info(id):
+
+    # create cursor
+    cursor = db.get_db().cursor()
+
+    # create query
+    query = f"""select Experience_level, Current_capacity, Age_group, Days_of_week, First_name, Last_name
+            from teams join employees e on e.ID = teams.Coach_ID
+            where teams.ID = '{id}';"""
+
+    # execute query
+    cursor.execute(query)
+
+    # return data as a json
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+
     return the_response
